570cd3a8d8dc70ab7f3165251fd72767c91f5478
SammI Fishbein <32447824+sammysam1198@users.noreply.github.com>
2026-03-07 21:10:33 -0500

    Update database connection to use environment variable

--- TKO-API/app.py
+++ TKO-API/app.py
@@ -29,7 +29,7 @@ def get_db_conn():
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-    return psycopg2.connect(database_url)
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
 
 def init_db():
