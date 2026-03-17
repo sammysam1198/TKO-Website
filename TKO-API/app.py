e1031cbf9885bb7d0319addd9c519023260688a4
SammI Fishbein <32447824+sammysam1198@users.noreply.github.com>
2026-03-16 21:24:04 -0400

    subscriber_bp -> subscribers_bp

--- TKO-API/app.py
+++ TKO-API/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 from db import init_db
 from routes.core_routes import core_bp
-from routes.subscriber_routes import subscriber_bp
+from routes.subscriber_routes import subscribers_bp
 from routes.post_routes import post_bp
 from routes.auth_routes import auth_bp
 from routes.dev_console_routes import dev_console_bp
@@ -14,7 +14,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(core_bp)
-app.register_blueprint(subscriber_bp)
+app.register_blueprint(subscribers_bp)
 app.register_blueprint(post_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(dev_console_bp)
